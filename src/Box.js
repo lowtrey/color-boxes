a464476bcb000808143a1849b306f7446304b65b
Q. Price <priceqp@gmail.com>
2019-06-15 18:56:27 -0500

    Create initial layout

---
+++ src/Box.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Box extends React.Component {
+    render() {
+        return (
+            <div style={{backgroundColor: 'blue', width: '100px', height: '100px'}} >
+                <h1>Box</h1>
+            </div>
+        )
+    }
+}
+
+export default Box;
---
+++ src/BoxContainer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Box from './Box';
+
+class BoxContainer extends React.Component {
+    render() {
+        return (
+            <div>
+                <Box />
+                <Box />
+                <Box />
+                <Box />
+                <Box />
+                <Box />
+                <Box />
+                <Box />
+                <Box />
+                <Box />
+            </div>
+        )
+    }
+}
+
+export default BoxContainer;
