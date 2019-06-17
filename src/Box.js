6795c8ef254a3464825dff162be0fe451469c5c9
Q. Price <priceqp@gmail.com>
2019-06-17 17:23:09 -0500

    Move state to container component, fix box render method

--- src/Box.js
+++ src/Box.js
@@ -3,23 +3,25 @@ import { choice } from './Helpers';
 import './Box.css';
 
 class Box extends React.Component {
-    static defaultProps = {
-        allColors: ['aqua', 'black', 'blue', 'fuchsia', 'gray', 'green', 
-        'lime', 'maroon', 'navy', 'olive', 'orange', 'purple', 'red', 
-        'silver', 'teal', 'white', 'yellow']
-    }
     constructor(props){
         super(props);
-        this.state = { color: choice(this.props.allColors) };
+        this.state = { color: choice(this.props.colors) };
         this.handleClick = this.handleClick.bind(this);
     }
+    pickColor(){
+        let newColor;
+        do {
+            newColor = choice(this.props.colors);
+        } while (newColor === this.state.color);
+        
+        this.setState({ color: newColor });
+    }
     handleClick(){
-
+        this.pickColor();
     }
     render() {
         return (
             <div className='Box' onClick={this.handleClick} style={{backgroundColor: this.state.color}} >
-                <h1>Box</h1>
             </div>
         )
     }
--- src/BoxContainer.js
+++ src/BoxContainer.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Box from './Box';
+import './BoxContainer.css';
 
 class BoxContainer extends React.Component {
+    static defaultProps = {
+        numBoxes: 18,
+        allColors: ['aqua', 'black', 'blue', 'fuchsia', 'gray', 'green', 
+        'lime', 'maroon', 'navy', 'olive', 'orange', 'purple', 'red', 
+        'silver', 'teal', 'white', 'yellow']
+    };
     render() {
-        return (
-            <div >
-                
-            </div>
-        )
+        const boxes = Array.from({ length: this.props.numBoxes }).map(() => (
+        <Box colors={this.props.allColors} />
+        ));
+        return <div className="BoxContainer">{boxes}</div>;
     }
 }
 
